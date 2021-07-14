b7339abef841276b5e83bc7b813b6696703ce3b0
carlosdoria <carlosdoria953@gmail.com>
2021-07-14 11:31:01 -0300

    feat: first commit with boilerplate structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+// import App from "next/app";
+import { AppProps /* , AppContext */ } from 'next/app'
+import Head from 'next/head'
+
+import '../styles/globals.scss'
+
+function MyApp ({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate</title>
+        <meta name="description" content="My boilerplete" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
