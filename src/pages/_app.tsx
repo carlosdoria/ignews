66f480a134b35f8a82837086939f3773b2a637f8
carlosdoria <carlosdoria953@gmail.com>
2021-07-20 15:22:14 -0300

    feat: add faunadb and next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,20 @@
 import { Header } from 'components/Header'
 import { AppProps /* , AppContext */ } from 'next/app'
 import Head from 'next/head'
+import { Provider as NextAuthProvider } from 'next-auth/client'
 
 import '../styles/globals.scss'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Head>
-        <title>Boilerplate</title>
+        <title>IgNews</title>
         <meta name="description" content="My boilerplete" />
       </Head>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
