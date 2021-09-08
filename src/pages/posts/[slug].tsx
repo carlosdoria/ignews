01bfbbbe43566e7ab7b04bf09f407313820edf8f
carlosdoria <carlosdoria953@gmail.com>
2021-09-08 19:10:14 -0300

    feat: create page preview

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -3,7 +3,6 @@ import { getSession } from 'next-auth/client'
 import Head from 'next/head'
 import { RichText } from 'prismic-dom'
 import { getPrismicClient } from '../../services/prismic'
-
 import styles from './post.module.scss'
 
 interface PostProps {
@@ -41,7 +40,7 @@ export const getServerSideProps: GetServerSideProps = async ({req, params}) => {
   const { slug } = params
 
   console.log(session)
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
